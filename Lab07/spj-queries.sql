9c8087858f08fd1422dfe04ff9b239f02f7c4699
kvlinden <kvlinden@calvin.edu>
2018-10-17 08:30:42 -0400

    updates for unit 7 Wednesday...

--- Lab07/spj-queries.sql
+++ Lab07/spj-queries.sql
@@ -1,24 +1,24 @@
 -- Get the number of suppliers.
 SELECT COUNT(*)
   FROM Supplier
-;
+  ;
 
 -- Get all data on all suppliers.
 SELECT *
   FROM Supplier
-;
+  ;
 
 -- Get the loginIDs for a stupid password.
 SELECT loginID
   FROM Supplier
  WHERE password = 'joshua'
-;
+ ;
 
 -- Get jobs without names.
 SELECT *
   FROM Job
  WHERE name IS NULL
-;
+ ;
 
 -- Get the names and quantities of parts supplied to jobs at Calvin.
 SELECT Part.name, PartJob.quantity
@@ -26,4 +26,4 @@ SELECT Part.name, PartJob.quantity
  WHERE Part.ID = PartJob.PartID
    AND PartJob.JobId = Job.ID
    AND Job.address = 'Calvin'
-;
+   ;
