33630f4f22e8a058d0721aa9b209a6660e4bec60
AJ Vrieland <zer0relm@gmail.com>
2020-10-02 15:39:42 -0400

    Lab05 finished

--- lab05/gamezone/shared/card.js
+++ lab05/gamezone/shared/card.js
@@ -3,10 +3,30 @@ import { StyleSheet, View } from 'react-native';
 
 export default function Card(props) {
     return (
-        <View>
-            <View>
+        <View style={styles.card}>
+            <View style={styles.cardContent}>
                 { props.children }
             </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 6,
+        elevation: 3,
+        backgroundColor: '#fff',
+        shadowOffset: {width: 1, height: 1},
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        // shadowsRadius: 2,
+        marginVertical: 6,
+        marginHorizontal: 4
+
+    },
+    cardContent: {
+        marginHorizontal: 18,
+        marginVertical: 10
+
+    }
+});
