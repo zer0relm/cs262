873620bf019a9de551f9987342b0d26ffc48643f
AJ Vrieland <zer0relm@gmail.com>
2020-10-19 16:53:19 -0400

    lab07 finished

--- lab07/monopoly.sql
+++ lab07/monopoly.sql
@@ -7,8 +7,10 @@
 
 -- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
 DROP TABLE IF EXISTS PlayerGame;
+DROP TABLE IF EXISTS DeedInfo;
 DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS Player;
+
 
 -- Create the schema.
 CREATE TABLE Game (
@@ -25,7 +27,17 @@ CREATE TABLE Player (
 CREATE TABLE PlayerGame (
 	gameID integer REFERENCES Game(ID), 
 	playerID integer REFERENCES Player(ID),
-	score integer
+	score integer,
+	money integer,
+	currentLocation varchar(50)
+	);
+
+CREATE TABLE DeedInfo (
+	gameID integer REFERENCES Game(ID), 
+	playerID integer REFERENCES Player(ID),
+	propertyName varchar(50),
+	houses integer,
+	hotels integer
 	);
 
 -- Allow users to select data from the tables.
@@ -37,6 +49,8 @@ GRANT SELECT ON PlayerGame TO PUBLIC;
 INSERT INTO Game VALUES (1, '2006-06-27 08:00:00');
 INSERT INTO Game VALUES (2, '2006-06-28 13:20:00');
 INSERT INTO Game VALUES (3, '2006-06-29 18:41:00');
+INSERT INTO Game VALUES (4, '2020-06-29 18:41:00');
+
 
 INSERT INTO Player(ID, emailAddress) VALUES (1, 'me@calvin.edu');
 INSERT INTO Player VALUES (2, 'king@gmail.edu', 'The King');
@@ -50,3 +64,11 @@ INSERT INTO PlayerGame VALUES (2, 2, 0.00);
 INSERT INTO PlayerGame VALUES (2, 3, 500.00);
 INSERT INTO PlayerGame VALUES (3, 2, 0.00);
 INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
+
+INSERT INTO PlayerGame VALUES (4, 1, 500.00, 1500, 'Baltic Avenue');
+INSERT INTO PlayerGame VALUES (4, 2, 0.00, 3, 'St. James Place');
+INSERT INTO PlayerGame VALUES (4, 3, 5500.00, 50, 'North Carolina Avenue');
+
+INSERT INTO DeedInfo VALUES (4, 1, 'North Carolina Avenue', 0, 4);
+INSERT INTO DeedInfo VALUES (4, 2, 'St. James Place', 3, 0);
+INSERT INTO DeedInfo VALUES (4, 1, 'Short Line', 0, 0);
